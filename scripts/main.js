4b962223c0e23bf7a1598c1634a213541da9a96d
mhawrysko1 <mhawrysko@gmail.com>
2019-10-21 06:24:23 -0400

    code fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
+myImage.onclick = function () {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
+    if (mySrc === 'images/firefox-icon.png') {
         myImage.setAttribute('src', 'images/firefox2.png');
     } else {
         myImage.setAttribute('src', 'images/firefox-icon.png');
     }
-}
+};
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
